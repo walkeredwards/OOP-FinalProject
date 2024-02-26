9aa1c578fb8acd75e6fbecc76aad07b8830e0986
elenasx <eeschmitt@mavs.coloradomesa.edu>
2024-02-26 15:11:50 -0700

    abstract

---
+++ project/chess.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python3
+
+import sys
+
+# Keeps track of where/which pieces are on the board and updates as 
+# players take turns with moves.
+class Board():
+
+
+class Piece():
+
+
+# Types of pieces which all have different move sets.
+class Pawn(Piece):
+
+
+class Bishop(Piece):
+
+
+class Knight(Piece):
+
+
+class Rook(Piece):
+
+
+class King(Piece):
+
+
+class Queen(Piece):
