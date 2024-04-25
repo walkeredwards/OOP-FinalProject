329623fa5ed81c70f53fdd85718661d5b682cacb
elenasx <89545479+elenasx@users.noreply.github.com>
2024-04-25 13:19:29 -0600

    Update pieces.py

--- project_v2/pieces.py
+++ project_v2/pieces.py
@@ -4,7 +4,7 @@ __authors__ = "Bryleigh Koci, Walker Edwards, Elena Schmitt"
 __date__ = "26 April 2024"
 __license__ = "MIT"
 
-""" Contains the pieces objects for chess game"""
+""" Contains the pieces objects for chess game."""
 
 
 from abc import abstractmethod
